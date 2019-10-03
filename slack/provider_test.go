e1bc911f69f731e7778fc443a9c48465e77c9ab3
int <kattu0426@gmail.com>
2019-10-04 05:08:49 +0900

    feat: add test init

--- slack/provider_test.go
+++ slack/provider_test.go
@@ -1,9 +1,34 @@
 package slack
 
 import (
+	"github.com/hashicorp/terraform/helper/schema"
 	"github.com/hashicorp/terraform/terraform"
+	"os"
 	"testing"
 )
+
+var testAccProviders map[string]terraform.ResourceProvider
+var testAccProviderFactories func(providers *[]*schema.Provider) map[string]terraform.ResourceProviderFactory
+var testAccProvider *schema.Provider
+var testAccProviderFunc func() *schema.Provider
+
+func init() {
+	testAccProvider = Provider()
+	testAccProviders = map[string]terraform.ResourceProvider{
+		"slack": testAccProvider,
+	}
+	testAccProviderFactories = func(providers *[]*schema.Provider) map[string]terraform.ResourceProviderFactory {
+		return map[string]terraform.ResourceProviderFactory{
+			"slack": func() (terraform.ResourceProvider, error) {
+				p := Provider()
+				*providers = append(*providers, p)
+				return p, nil
+			},
+		}
+	}
+
+	testAccProviderFunc = func() *schema.Provider { return testAccProvider }
+}
 
 func TestProvider(t *testing.T) {
 	if err := Provider().InternalValidate(); err != nil {
@@ -13,4 +38,18 @@ func TestProvider(t *testing.T) {
 
 func TestProvider_impl(t *testing.T) {
 	var _ terraform.ResourceProvider = Provider()
+}
+
+func testAccPreCheck(t *testing.T) {
+	if os.Getenv("SLACK_TOKEN") == "" {
+		t.Fatal("SLACK_TOKEN must be set for acceptance tests")
+	}
+	if os.Getenv("SLACK_TOKEN") != "" {
+		t.Fatal("SLACK_TOKEN must be set for acceptance tests")
+	}
+
+	err := testAccProvider.Configure(terraform.NewResourceConfigRaw(nil))
+	if err != nil {
+		t.Fatal(err)
+	}
 }
