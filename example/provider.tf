945bea58a5d3b770f6b4dc05ee0df55a190da87d
int <kattu0426@gmail.com>
2019-10-17 00:25:17 +0900

    doc: update sample

--- example/provider.tf
+++ example/provider.tf
@@ -2,6 +2,15 @@ provider "slack" {
   token = ""
 }
 
+data "slack_user" "user" {
+  id = "UPFA0RTA4"
+}
+
 resource "slack_channel" "test" {
-  name = "test-channel"
+  name = "test-channel-invite-test-06"
+}
+
+resource "slack_channel_invite" "invite" {
+  channel_id = slack_channel.test.id
+  user_id = data.slack_user.user.id
 }
